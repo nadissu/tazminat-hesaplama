81326e90892686268777b55ca57b26d43b30f9e0
nadissu <nadidesigirtmac@gmail.com>
2026-01-31 17:08:31 +0300

    Adsense Commeited

--- server.js
+++ server.js
@@ -12,6 +12,28 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
+// Legal pages
+app.get('/gizlilik', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'gizlilik.html'));
+});
+
+app.get('/kullanim-sartlari', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'kullanim-sartlari.html'));
+});
+
+app.get('/cerez-politikasi', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'cerez-politikasi.html'));
+});
+
+// Info pages
+app.get('/hakkimizda', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'hakkimizda.html'));
+});
+
+app.get('/iletisim', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'iletisim.html'));
+});
+
 // Start server
 app.listen(PORT, () => {
     console.log(`Server running at http://localhost:${PORT}`);
